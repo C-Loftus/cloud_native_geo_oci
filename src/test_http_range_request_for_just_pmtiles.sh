c02fde8231160e3aba51b1683e464061fa7091d6
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2026-06-01 09:53:48 -0400

    add a test script

---
+++ src/test_http_range_request_for_just_pmtiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get an ephemeral, anonymous token
+ANONYMOUS_TOKEN=$(curl -s "https://ghcr.io/token?scope=repository:c-loftus/cloud_native_geo_oci:pull" | jq -r .token)
+
+echo "Testing http range request"
+curl -v -L \
+  -H "Authorization: Bearer $ANONYMOUS_TOKEN" \
+  -H "Range: bytes=0-511" \
+  "https://ghcr.io/v2/c-loftus/cloud_native_geo_oci/blobs/sha256:d6f19caf2b4c2b46daff17865a83a4ea19a19c7c60599a86b711f5639f9c750b"
